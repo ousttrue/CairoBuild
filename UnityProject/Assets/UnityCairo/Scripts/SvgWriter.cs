ee7fb45204c21f24f1a343d07bb58693f33ebefa
ousttrue <ousttrue@gmail.com>
2018-10-18 10:58:39 +0900

    Implement svg rect
    http://tutorials.jenkov.com/svg/simple-svg-example.html

---
+++ UnityProject/Assets/UnityCairo/Scripts/SvgWriter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace UnityCairo
+{
+    static class SvgWriter
+    {
+        struct Rgb
+        {
+            public double r;
+            public double g;
+            public double b;
+
+            public static Rgb Parse(string rgb)
+            {
+                if (rgb[0] != '#') throw new Exception();
+                if (rgb.Length != 7) throw new Exception();
+                return new Rgb
+                {
+                    r = Convert.ToByte(rgb.Substring(1, 2), 16) / 255.0,
+                    g = Convert.ToByte(rgb.Substring(3, 2), 16) / 255.0,
+                    b = Convert.ToByte(rgb.Substring(5, 2), 16) / 255.0,
+                };
+            }
+        }
+
+        struct Style
+        {
+            public Rgb? Stroke;
+            public Rgb? Fill;
+
+            public static Style Parse(string src)
+            {
+                var style = new Style
+                {
+                };
+
+                foreach (var token in src.Split(';'))
+                {
+                    var splited = token.Split(':');
+                    switch (splited[0].Trim())
+                    {
+                        case "stroke":
+                            style.Stroke = Rgb.Parse(splited[1].Trim());
+                            break;
+
+                        case "fill":
+                            style.Fill = Rgb.Parse(splited[1].Trim());
+                            break;
+                    }
+                }
+
+                return style;
+            }
+        }
+
+        static void DrawRect(XElement e, Cairo cr)
+        {
+            var x = double.Parse(e.Attribute("x").Value);
+            var y = double.Parse(e.Attribute("y").Value);
+            var w = double.Parse(e.Attribute("width").Value);
+            var h = double.Parse(e.Attribute("height").Value);
+            var style = Style.Parse(e.Attribute("style").Value);
+            cr.rectangle(x, y, w, h);
+            if (style.Fill.HasValue)
+            {
+                var rgb = style.Fill.Value;
+                cr.set_source_rgb(rgb.r, rgb.g, rgb.b);
+                if (style.Stroke.HasValue)
+                {
+                    cr.fill_preserve();
+                }
+                else
+                {
+                    cr.fill();
+                }
+            }
+            if (style.Stroke.HasValue)
+            {
+                var rgb = style.Stroke.Value;
+                cr.set_source_rgb(rgb.r, rgb.g, rgb.b);
+                cr.stroke();
+            }
+        }
+
+        public static void Draw(XElement e, Cairo cr)
+        {
+            switch (e.Name.LocalName)
+            {
+                case "rect":
+                    DrawRect(e, cr);
+                    break;
+
+                default:
+                    throw new NotImplementedException(e.ToString());
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/UnityCairo/Scripts/WriteSvg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Xml.Linq;
+using UnityCairo;
+using UnityEngine;
+
+
+public class WriteSvg : MonoBehaviour
+{
+    [SerializeField, TextArea(4, 24)]
+    string m_svg = @"<svg  xmlns=""http://www.w3.org/2000/svg""
+      xmlns:xlink=""http://www.w3.org/1999/xlink"">
+    <rect x = ""10"" y=""10"" height=""100"" width=""100""
+          style=""stroke:#ff0000; fill: #0000ff""/>
+</svg>
+";
+
+    [SerializeField]
+    string m_url = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ghostscript_Tiger.svg";
+
+    [SerializeField]
+    bool m_useUrl;
+
+    [SerializeField]
+    int m_width = 512;
+
+    [SerializeField]
+    int m_height = 512;
+
+    [SerializeField]
+    Shader m_shader;
+
+    private void Reset()
+    {
+        m_shader = Shader.Find("Unlit/Transparent");
+    }
+
+    public Texture2D Texture;
+
+    private void OnValidate()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        StopAllCoroutines();
+
+        StartCoroutine(WriteSVG());
+    }
+
+    IEnumerator WriteSVG()
+    {
+        if (m_useUrl)
+        {
+            var www = new WWW(m_url);
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                yield break;
+            }
+            m_svg = www.text;
+        }
+
+        var material = new Material(m_shader);
+
+        Texture = new Texture2D(m_width, m_height, TextureFormat.BGRA32, false);
+        material.mainTexture = Texture;
+        var data = new Byte[m_width * m_height * 4];
+
+        using (var surface = Surface.CreateFromBytes(data, m_width, m_height, m_width * 4))
+        {
+            //using (var surface = Surface.Create(120, 120))
+            using (var cairo = Cairo.Create(surface))
+            {
+                var document = XDocument.Parse(m_svg);
+                foreach (var x in document.Root.Elements())
+                {
+                    SvgWriter.Draw(x, cairo);
+                }
+            }
+
+            Texture.LoadRawTextureData(data);
+            Texture.Apply();
+        }
+
+        var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        var renderer = quad.GetComponent<Renderer>();
+        renderer.sharedMaterial = material;
+    }
+}
